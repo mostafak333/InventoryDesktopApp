b91b313f5984998822c17d8ea4333f8c864c4f6f
Mustafa kamal <mostafakamalhassan0@gmail.com>
2026-02-03 21:28:36 +0200

    Initial commit with Drift setup

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const InventoryApp());
+}
+
+class InventoryApp extends StatelessWidget {
+  const InventoryApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Inventory System',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home'),
+      ),
+      body: const Center(
+        child: Text(
+          'Inventory App Running 🚀',
+          style: TextStyle(fontSize: 22),
+        ),
+      ),
+    );
+  }
+}
